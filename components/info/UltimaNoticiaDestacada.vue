7a92e6dd509c18ed9e738b084e6718f162fb3157
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-06 13:19:11 -0400

    chore: ultima noticia destacada with real data

--- components/info/UltimaNoticiaDestacada.vue
+++ components/info/UltimaNoticiaDestacada.vue
@@ -3,11 +3,48 @@
     <v-carousel
       v-if="$store.state.noticias.featuredNoticias.data"
       hide-delimiters
+      height="auto"
     >
-      <v-carousel-item v-for="(noticiaDestacada, index) in $store.state.noticias.featuredNoticias.data" :key="color">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">{{ noticiaDestacada.title }}</div>
-          </v-row>
+      <v-carousel-item
+        v-for="(noticiaDestacada, index) in $store.state.noticias
+          .featuredNoticias.data"
+        :key="color"
+      >
+        <v-card
+          dark
+          :style="{
+            backgroundImage: `url(${
+              $config.apiAssetsV2 +
+              noticiaDestacada.featured_image.filename_disk
+            })`,
+          }"
+          class="bg-fixed bg-cover"
+        >
+          <div class="bg-slate-800/40 py-10 py-md-0">
+            <v-container>
+              <v-responsive
+                :aspect-ratio="16 / 6"
+                container-class=""
+                content-class="flex align-center"
+              >
+                <div>
+                  <div class="text-h3 font-weight-bold">
+                    {{ noticiaDestacada.title }}
+                  </div>
+                  <div class="font-sans text-xl-2">
+                    {{ noticiaDestacada.excerpt }}
+                  </div>
+                  <v-btn
+                    class="mt-5"
+                    outlined
+                    :to="`${'/noticias/' + noticiaDestacada.slug}`"
+                    >Leer más</v-btn
+                  >
+                </div>
+              </v-responsive>
+            </v-container>
+          </div>
+        </v-card>
       </v-carousel-item>
     </v-carousel>
     <!-- <v-carousel
@@ -73,12 +110,7 @@
 </template>
 <script>
 export default {
-  data() {
-    return {
-      model: 0,
-      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
-    };
-  },
+
 };
 </script>
 <style lang=""></style>
