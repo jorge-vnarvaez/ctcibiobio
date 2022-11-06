d1fbd33831964d30bc4b51fe0910c6b360b9e24e
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-06 11:58:48 -0400

    chore: derivadores globales images

--- components/info/DerivadoresGlobales.vue
+++ components/info/DerivadoresGlobales.vue
@@ -4,21 +4,21 @@
       <v-responsive :aspect-ratio="16/6" class="flex align-center">
         <div class="d-flex flex-column flex-md-row my-10 justify-center mx-n3">
             <div class="mx-3">
-              <v-img width="60px" height="60px" contain class="mb-5" src="/icons/modelo.png"></v-img>
+              <v-img width="60px" height="60px" class="mb-5" src="/iconos/modelo.png"></v-img>
               <div class="text-xl font-weight-bold text-slate-800 mb-5">Explora el modelo estratégico</div>
               <nuxt-link to="/estrategia">
                 <div class="text-base font-weight-bold text-slate-400">Ver modelo</div>
               </nuxt-link>
             </div>
             <div class="mx-3">
-              <v-img width="60px" height="60px" contain class="mb-5" src="/icons/calendario.png"></v-img>
+              <v-img width="60px" height="60px" class="mb-5" src="/iconos/calendario.png"></v-img>
               <div class="text-xl font-weight-bold text-slate-800 mb-5">Participa de las actividades del proyecto</div>
               <nuxt-link to="/eventos">
                 <div class="text-base font-weight-bold text-slate-400">Revisar calendario</div>
               </nuxt-link>
             </div>
             <div class="mx-3">
-              <v-img width="60px" height="60px" contain class="mb-5" src="/icons/grupo.png"></v-img>
+              <v-img width="60px" height="60px"  class="mb-5" src="/iconos/grupo.png"></v-img>
               <div class="text-xl font-weight-bold text-slate-800 mb-5">Conoce el equipo gestor del proyecto</div>
               <nuxt-link to="/equipo">
                 <div class="text-base font-weight-bold text-slate-400">Conocer equipo</div>
