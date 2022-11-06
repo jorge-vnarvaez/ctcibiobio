13759bbedb75b32cc8f5a25df38da618b37b1aaa
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-06 12:36:16 -0400

    chore: ultima noticia destacada

--- components/info/UltimaNoticiaDestacada.vue
+++ components/info/UltimaNoticiaDestacada.vue
@@ -1,15 +1,6 @@
 <template lang="">
   <div>
-    <v-carousel v-model="model">
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">Slide {{ i + 1 }}</div>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-    <!-- <v-carousel
+    <v-carousel
       class="blue-grey lighten-5"
       :continuous="true"
       :cycle="true"
@@ -67,23 +58,12 @@
             </div>
         </v-card>
       </v-carousel-item>
-    </v-carousel> -->
+    </v-carousel>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
-    }
-  }
+  
 };
 </script>
 <style lang=""></style>
