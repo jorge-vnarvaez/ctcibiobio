a9ec18819408d2818e6442bff458933b3145d75e
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-06 13:07:47 -0400

    chore: v-carousel with featured notices title

--- components/info/UltimaNoticiaDestacada.vue
+++ components/info/UltimaNoticiaDestacada.vue
@@ -1,12 +1,13 @@
 <template lang="">
   <div>
-    <v-carousel v-model="model">
-      <v-carousel-item v-for="(color, i) in colors" :key="color">
-        <v-sheet :color="color" height="100%" tile>
+    <v-carousel
+      v-if="$store.state.noticias.featuredNoticias.data"
+      hide-delimiters
+    >
+      <v-carousel-item v-for="(noticiaDestacada, index) in $store.state.noticias.featuredNoticias.data" :key="color">
           <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">Slide {{ i + 1 }}</div>
+            <div class="text-h2">{{ noticiaDestacada.title }}</div>
           </v-row>
-        </v-sheet>
       </v-carousel-item>
     </v-carousel>
     <!-- <v-carousel
@@ -75,15 +76,9 @@ export default {
   data() {
     return {
       model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
-    }
-  }
+      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
+    };
+  },
 };
 </script>
 <style lang=""></style>
