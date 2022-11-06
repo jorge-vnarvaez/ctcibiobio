71aae6916e0f67f881d86a2447a808286527a8a3
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-11-06 12:09:16 -0400

    testing: v-responsive on index

--- components/info/DerivadoresGlobales.vue
+++ components/info/DerivadoresGlobales.vue
@@ -4,7 +4,7 @@
       <v-responsive :aspect-ratio="16/6" class="flex align-center">
         <div class="d-flex flex-column flex-md-row my-10 justify-center mx-n3">
             <div class="mx-3">
-              <v-img width="60px" height="60px" class="mb-5" src="/iconos/modelo.png"></v-img>
+              <v-img width="60px" height="60px" class="mb-5" contain src="/iconos/modelo.png"></v-img>
               <div class="text-xl font-weight-bold text-slate-800 mb-5">Explora el modelo estratégico</div>
               <nuxt-link to="/estrategia">
                 <div class="text-base font-weight-bold text-slate-400">Ver modelo</div>
