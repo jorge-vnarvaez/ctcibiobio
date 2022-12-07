f04b5d7944ffabc5bb641c1b3cfc8211eb7ed628
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2022-12-07 14:49:58 -0400

    add: youtube video of sofia valuenzuela

--- components/info/YoutubeDeliveries.vue
+++ components/info/YoutubeDeliveries.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="max-w-screen-xl mx-auto pt-20">
     <v-container>
-        <UtilitariosVideo src="https://www.youtube.com/embed/ggQHr_31SVY" />
+      <UtilitariosVideo
+        src="https://www.youtube.com/embed/ggQHr_31SVY"
+        txt="Intervención de <span class='font-bold'>Iván Valenzuela, Jefe de División de Fomento e Industria
+      GORE Biobío</span>, en el Taller Inicial de la Estrategia de CTCI Biobío 2022,
+      destacando la importancia de esta estrategia para el desarrollo de la
+      región del Biobío, que se logrará con colaboración, capital humano y todas
+      las capacidades que posee nuestra región."
+      />
+
+      <UtilitariosVideo
+      class="mt-20"
+        reverse
+        src="https://www.youtube.com/embed/t1-6hozWx0k"
+        txt="Intervención de la Seremi de Ciencia, Tecnología, Conocimiento e Innovación Macrozona Sur, Sofía Valenzuela, en el Taller de Mujeres para la Construcción de la Estrategia de CTCI Biobío 2022. "
+      />
     </v-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
